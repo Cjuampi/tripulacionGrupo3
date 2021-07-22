55113865cc0455f4308ebbe3d0dc361f55f6194b
AntonioAp <antonioaparicioh@gmail.com>
2021-07-22 15:59:12 +0200

    App conflicts resolved again

--- client/front-reto/src/App.js
+++ client/front-reto/src/App.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter} from 'react-router-dom';
-=======
-import React, { useState } from "react";
->>>>>>> acee8aa79d9a506d9efa8098345794a9b7d82d55
 import './App.css';
 //import Map from './components/Map/Map';
 import "leaflet/dist/leaflet.css";
